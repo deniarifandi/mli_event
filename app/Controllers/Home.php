5b4936b786bd5ac92f75dc12201465ddb97add25
deniarifandi <arifandi.malang@gmail.com>
2024-11-07 23:11:49 +0700

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -74,7 +74,7 @@ class Home extends BaseController
         $config["SMTPPass"]  = "n@PnMwkB#k3@";
         $config["SMTPPort"]  = 465;
         $config["SMTPCrypto"] = "ssl";
-        $config['protocol'] = "mail";
+      
         $config['smtp_port'] = 587;
 
         $email_smtp->initialize($config);
