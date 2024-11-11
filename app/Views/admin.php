2392cee55af60542e8774bde8866930abb9ed6a3
deniarifandi <arifandi.malang@gmail.com>
2024-11-11 14:01:05 +0700

    Update admin.php

--- app/Views/admin.php
+++ app/Views/admin.php
@@ -31,6 +31,7 @@
                   <th>Occupation</th>
                   <th>Register Time</th>
                   <th>Status Pengiriman Tiket</th>
+                  <th>Nomor Tiket</th>
                   <th>Send Ticket</th>
               </tr>
           </thead>
@@ -50,6 +51,7 @@
                     ?><button class="btn btn-sm btn-success" disabled>Tiket Sudah Dikirim</button><?php
                   }
                   ?></td>
+                  <td><?php echo $admin[$i]->ticket_no; ?></td>
                   <td><a class="btn btn-sm btn-primary" href="<?php echo base_url()?>send_ticket?nama=<?php echo $admin[$i]->nama; ?>&email=<?php echo $admin[$i]->email?>">Send Ticket</a></td>
                 </tr>
             <?php } ?>
